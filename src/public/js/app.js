33279bc3d93f030fa186246a4a85589bb064a24b
d0422 <rlfehd2013@naver.com>
2022-09-20 02:16:08 +0900

    2.3

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,37 +1,13 @@
-const messageList = document.querySelector("ul");
-const nickForm = document.querySelector("#nickname");
-const messageForm = document.querySelector("#message");
-var socket = new WebSocket(`ws://${window.location.host}`);
-
-function makeMessage(type, payload) {
-  const msg = { type, payload };
-  return JSON.stringify(msg);
-}
-
-// 소켓 연결은 ws://로 한다.
-socket.addEventListener("open", () => {
-  console.log("connected to Server");
-});
-socket.addEventListener("message", (message) => {
-  const li = document.createElement("li");
-  li.innerHTML = message.data;
-  messageList.append(li);
-});
-// 서버에서 받은 데이터 표기
-socket.addEventListener("close", () => {
-  console.log("서버와 연결이 끊어졌습니다.");
-});
-function handleSubmit(event) {
+const socket = io();
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+function handleRoomSubmit(event) {
   event.preventDefault();
-  const input = messageForm.querySelector("input");
-  socket.send(makeMessage("new_message", input.value));
+  const input = form.querySelector("input");
+  socket.emit("enter_room", { payload: input.value }, () => {
+    console.log("server is done");
+  });
   input.value = "";
 }
-function handleNickSubmit(event) {
-  event.preventDefault();
-  const input = nickForm.querySelector("input");
-  socket.send(makeMessage("nickname", input.value));
-  input.value = "";
-}
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+
+form.addEventListener("submit", handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import http from "http";
-import ws, { WebSocketServer } from "ws";
+import SoketIO from "socket.io";
+
 const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
@@ -10,40 +11,49 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 // 어떤 url을 입력하든 home으로 리다이렉트시킴
 
-const server = http.createServer(app); // requestListener가 필요함
-const wss = new WebSocketServer({ server });
+const httpServer = http.createServer(app); // requestListener가 필요함
+const wsServer = SoketIO(httpServer);
+wsServer.on("connection", (socket) => {
+  socket.on("enter_room", (msg, done) => {
+    console.log(msg);
+    setTimeout(() => {
+      done();
+    }, 2000);
+  });
+});
+// const wss = new WebSocketServer({ server });
 // 같은 서버에서 http, webSocket 둘다 작동(두개가 같은 포트에서 작동함)
 // http서버 위에 wss를 만든 것
 
-const sockets = [];
+// const sockets = [];
 
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "Anon";
-  // 소켓연결을 sockets에 보관
-  console.log("연결됨");
-  socket.send("hello!!");
-  // hello라는 데이터를 소켓에 담아서 보내줌
-  socket.on("close", () => {
-    console.log("프론트 연결 끊김");
-  });
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg);
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((aSocket) => {
-          aSocket.send(
-            `${socket.nickname}:${message.payload.toString("utf8")}`
-          );
-        });
-        // 연결된 소켓에서 메시지가 오는 경우, 모든 socket에 대해 메시지 전송해줌
-        break;
-      case "nickname":
-        socket["nickname"] = message.payload;
-        break;
-    }
-  });
-  // 연결이 끊겼을때의 상황
-});
+// wss.on("connection", (socket) => {
+//   sockets.push(socket);
+//   socket["nickname"] = "Anon";
+//   // 소켓연결을 sockets에 보관
+//   console.log("연결됨");
+//   socket.send("hello!!");
+//   // hello라는 데이터를 소켓에 담아서 보내줌
+//   socket.on("close", () => {
+//     console.log("프론트 연결 끊김");
+//   });
+//   socket.on("message", (msg) => {
+//     const message = JSON.parse(msg);
+//     switch (message.type) {
+//       case "new_message":
+//         sockets.forEach((aSocket) => {
+//           aSocket.send(
+//             `${socket.nickname}:${message.payload.toString("utf8")}`
+//           );
+//         });
+//         // 연결된 소켓에서 메시지가 오는 경우, 모든 socket에 대해 메시지 전송해줌
+//         break;
+//       case "nickname":
+//         socket["nickname"] = message.payload;
+//         break;
+//     }
+//   });
+//   // 연결이 끊겼을때의 상황
+// });
 
-server.listen(3000);
+httpServer.listen(3000);
